b47fbbed253a5bfb79d72779daf53f9fb272c821
kris <kris@sbit.io>
2022-02-09 11:41:13 +0100

    fix puppet-lint: indentation of => is not properly aligned for internallist_entry.pp

--- manifests/internallist_entry.pp
+++ manifests/internallist_entry.pp
@@ -1,11 +1,11 @@
 define dkim_filter::internallist_entry (
-  $ensure   = present,
+  $ensure = present,
 ) {
 
   dkim_filter::hostsnets_entry { "internalhosts_${name}":
-      ensure  => $ensure,
-      file    => $dkim_filter::params::internallist_file,
-      entry   => $name,
+      ensure => $ensure,
+      file   => $dkim_filter::params::internallist_file,
+      entry  => $name,
   }
 
 }
