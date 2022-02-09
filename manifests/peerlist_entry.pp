3e623c018495d207c2b52552e0aa1ca3793c0857
kris <kris@sbit.io>
2022-02-09 12:01:11 +0100

    fix puppet-lint: indentation of => is not properly aligned for peerlist_entry.pp

--- manifests/peerlist_entry.pp
+++ manifests/peerlist_entry.pp
@@ -1,11 +1,11 @@
 define dkim_filter::peerlist_entry (
-  $ensure   = present,
+  $ensure = present,
 ) {
 
   dkim_filter::hostsnets_entry { "peer_${name}":
-      ensure  => $ensure,
-      file    => $dkim_filter::params::peerlist_file,
-      entry   => $name,
+      ensure => $ensure,
+      file   => $dkim_filter::params::peerlist_file,
+      entry  => $name,
   }
 
 }
