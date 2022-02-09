5315550ee3a114fdd2842a8346b0f1692af57763
kris <kris@sbit.io>
2022-02-09 12:00:24 +0100

    fix puppet-lint: indentation of => is not properly aligned for exitignore_entry.pp

--- manifests/extignore_entry.pp
+++ manifests/extignore_entry.pp
@@ -1,11 +1,11 @@
 define dkim_filter::extignore_entry (
-  $ensure   = present,
+  $ensure = present,
 ) {
 
   dkim_filter::hostsnets_entry { "extignore_hosts_${name}":
-      ensure  => $ensure,
-      file    => $dkim_filter::params::extignore_file,
-      entry   => $name,
+      ensure => $ensure,
+      file   => $dkim_filter::params::extignore_file,
+      entry  => $name,
   }
 
 }
